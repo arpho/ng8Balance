1d4786a6cebc0f2261cb6f7cea81df5e4e105eb9
Giuseppe <damicogiuseppe77@gmail.com>
2020-01-25 18:57:57 +0100

    ngrx/store
    added ngrx/store and reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { reducers, metaReducers } from './reducers';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -8,13 +9,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { ItemModule } from '../app/modules/item/item.module'
 import { GeoLocationModule } from './modules/geo-location/geo-location.module';
 import { DynamicFormModule } from './modules/dynamic-form/dynamic-form.module';
-
+import { Storage, IonicStorageModule } from '@ionic/storage';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { FilterPopupPage } from './modules/item/pages/filter-popup/filter-popup.page';
 import { SelectorItemsPage } from './modules/item/pages/selector-items/selector-items.page';
+import { GoogleChartModule} from 'angular-google-chart'
+import {StoreModule} from '@ngrx/store'
 
 @NgModule({
   declarations: [AppComponent, FilterPopupPage, SelectorItemsPage],
@@ -28,8 +31,11 @@ import { SelectorItemsPage } from './modules/item/pages/selector-items/selector-
     GeoLocationModule,
     BrowserModule,
     IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
+    // GoogleChartModule.forRoot(),
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    StoreModule.forRoot(reducers, { metaReducers }),
   ],
   providers: [
     StatusBar,
