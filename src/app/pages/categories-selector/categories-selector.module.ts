ba0a793d4893ad9c0ed267edfc6a966b77c1811e
Giuseppe <damicogiuseppe77@gmail.com>
2020-02-01 16:38:45 +0100

    ion-searchbar
    the searchbar in categoriesSelector works

--- src/app/pages/categories-selector/categories-selector.module.ts
+++ src/app/pages/categories-selector/categories-selector.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
@@ -22,6 +22,7 @@ const routes: Routes = [
     IonicModule,
     ItemModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
   // declarations: [CategoriesSelectorPage]
