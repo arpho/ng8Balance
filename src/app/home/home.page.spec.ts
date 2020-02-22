cb2ee1b4fcc06647c068f97d86aaca7efff21d45
Giuseppe <damicogiuseppe77@gmail.com>
2020-02-22 17:12:10 +0100

    refactoring test
    imported online-status module on home.page.spec

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { OnlineStatusModule } from '../modules/online-status/online-status.module';
 
 describe('HomePage', () => {
   let component: HomePage;
@@ -10,7 +11,7 @@ describe('HomePage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(),OnlineStatusModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
