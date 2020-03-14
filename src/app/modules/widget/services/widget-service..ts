6a11bf526469a7bd460a390a80f9403eccfa1560
Giuseppe <damicogiuseppe77@gmail.com>
2020-03-14 16:41:47 +0100

    refactoring
    implemented constant storeName in widgetService

--- src/app/modules/widget/services/widget-service..ts
+++ src/app/modules/widget/services/widget-service..ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { openDB, deleteDB, wrap, unwrap, IDBPDatabase } from 'idb'
-import { Observable,Subject, BehaviorSubject } from 'rxjs';
-import {Schedule} from '../../../app.component'
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { Schedule } from '../../../app.component'
 import { promise } from 'protractor';
 
 
@@ -10,32 +10,33 @@ import { promise } from 'protractor';
 })
 export class WidgetService {
   private _dataChange: Subject<Schedule> = new Subject<Schedule>();
-private _db 
-private _widgets:BehaviorSubject<Promise<any>> = new  BehaviorSubject(new Promise((success,fail)=>{success(undefined)}))
-public readonly widgets:Observable<any>=  this._widgets.asObservable()
+  private storeName = 'widgetsList'
+  private _db
+  private _widgets: BehaviorSubject<Promise<any>> = new BehaviorSubject(new Promise((success, fail) => { success(undefined) }))
+  public readonly widgets: Observable<any> = this._widgets.asObservable()
 
-constructor(public service:WidgetService) {
-  // this.storage.keys().then(keys=>keys.forEach(k=>this.get(k).then(v=>console.log('value for ',k,v))))
-  
-  this.connectToIDB()
-}
+  constructor(public service: WidgetService) {
+    // this.storage.keys().then(keys=>keys.forEach(k=>this.get(k).then(v=>console.log('value for ',k,v))))
+
+    this.connectToIDB()
+  }
 
   keys(next) {
-    this.widgets.subscribe(db=>{
-      console.log('db',db)
-      if (db.getAllKeys){
-        next(db.getAllKeys('widgetsList'))
+    this.widgets.subscribe(db => {
+      console.log('db', db)
+      if (db.getAllKeys) {
+        next(db.getAllKeys(this.storeName))
         // return db.getAllKeys('widgetsList')
       }
     })
-     
+
   }
 
   set(key: string, value) {
     // this.storage.set(key, value)
   }
 
-  get(key:string){
+  get(key: string) {
     // return this.storage.get(key)
   }
 
@@ -43,7 +44,7 @@ constructor(public service:WidgetService) {
     this._db = await openDB('widgets', 2.7, {
       upgrade(db, oldVersion, newVersion, transaction) {
         console.log(`updating db:${db}, oldVersion:${oldVersion},newVersion:${newVersion},transaction:${transaction}`)
-        db.createObjectStore('widgetsList',{keyPath:'id',autoIncrement:true})
+        db.createObjectStore(this.storeName, { keyPath: 'id', autoIncrement: true })
 
       },
       blocked() {
@@ -62,5 +63,5 @@ constructor(public service:WidgetService) {
     this._widgets.next(this._db)
   }
 
-  
+
 }
