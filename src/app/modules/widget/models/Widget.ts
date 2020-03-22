baa436dd38134f60aede620e7fa2306bddd3f6f0
Giuseppe <damicogiuseppe77@gmail.com>
2020-03-22 17:17:42 +0100

    refactoring
    widget not implements itemModelInterface

--- src/app/modules/widget/models/Widget.ts
+++ src/app/modules/widget/models/Widget.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject } from 'rxjs';
 import { Value } from '../../item/models/value';
 import { ItemModelInterface } from '../../item/models/itemModelInterface';
 
-export class Widget implements ItemModelInterface {
+export class Widget  {
     constructor(args?: { service: EntityWidgetServiceInterface, entityKey: string, period: number, counter: boolean, _order: number }) {
         this.value = new BehaviorSubject(new Value({ value: 0, label: this.service ? this.service.entitityLabel : 'testing' }))
         this.load(args)
@@ -20,39 +20,6 @@ export class Widget implements ItemModelInterface {
 
         }
         return new Value({ label: 'widget', value: 'totale' })
-    }
-    getCountingText(): string {
-        throw new Error("Method not implemented.");
-    }
-    getNote(): Value {
-        throw new Error("Method not implemented.");
-    }
-    archiveItem?(b: boolean) {
-        throw new Error("Method not implemented.");
-    }
-    isArchivable?(): boolean {
-        throw new Error("Method not implemented.");
-    }
-    getValue2(): Value {
-        throw new Error("Method not implemented.");
-    }
-    getValue3(): Value {
-        throw new Error("Method not implemented.");
-    }
-    getValue4(): Value {
-        throw new Error("Method not implemented.");
-    }
-    getEditPopup(item?: ItemModelInterface, serservice?: import("../../item/models/ItemServiceInterface").ItemServiceInterface) {
-        throw new Error("Method not implemented.");
-    }
-    getAggregate(): Value {
-        throw new Error("Method not implemented.");
-    }
-    hasQuickActions?(): boolean {
-        throw new Error("Method not implemented.");
-    }
-    getElement(): { element: string; genere: import("../../item/models/itemModelInterface").Genere; } {
-        throw new Error("Method not implemented.");
     }
     service: EntityWidgetServiceInterface
     private period: number
