f6088a5a1d22689403514dfbabeb2ea10e036b74
Giuseppe <damicogiuseppe77@gmail.com>
2020-05-02 17:49:11 +0200

    test working
    test for new components ok, 
    #WARNING: some text related to ibc do not work

--- src/app/modules/widget/components/widget/widget.component.spec.ts
+++ src/app/modules/widget/components/widget/widget.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { WidgetComponent } from './widget.component';
+import { UtilitiesModule } from 'src/app/modules/utilities/utilities.module';
 
 describe('WidgetComponent', () => {
   let component: WidgetComponent;
@@ -9,8 +10,8 @@ describe('WidgetComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WidgetComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [WidgetComponent],
+      imports: [IonicModule.forRoot(), UtilitiesModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(WidgetComponent);
