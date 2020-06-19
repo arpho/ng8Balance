5f30da6465c0b81a71696bce764a294e8d33a830
Giuseppe <damicogiuseppe77@gmail.com>
2020-06-19 17:46:32 +0200

    editUserPage.spec
    provided MockNavParams

--- src/app/modules/user/pages/edit-user/edit-user.page.spec.ts
+++ src/app/modules/user/pages/edit-user/edit-user.page.spec.ts
@@ -2,6 +2,8 @@ import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { EditUserPage } from "./edit-user.page";
+import { ModalController, AngularDelegate, NavParams } from '@ionic/angular';
+import { MockNavParams } from './mockNavParams';
 
 describe("EditUserPage", () => {
   let component: EditUserPage;
@@ -11,7 +13,8 @@ describe("EditUserPage", () => {
     TestBed.configureTestingModule({
       declarations: [EditUserPage],
       imports: [RouterTestingModule.withRoutes([])],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers:[ModalController,AngularDelegate, { provide: NavParams, useClass: MockNavParams }]
     }).compileComponents();
   }));
 
