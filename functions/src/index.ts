bc660a15ebe3c936cd0aa43787e638c4d3a7ffee
Giuseppe <damicogiuseppe77@gmail.com>
2020-07-01 18:54:38 +0200

    triggerUsers
    implemented  firebase function for updating users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,48 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
- export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+
+
+const getUpdatedUser = (event: any) => {
+  const compare = (obj1: any, obj2: any) =>
+    JSON.stringify(obj1) === JSON.stringify(obj2);
+  return Object.keys(event.after.val()).filter(
+    key => !compare(event.after.val()[key], event.before.val()[key])
+  );
+};
+const setClaims = async (data: {
+  email: string;
+  level: number;
+  enabled: boolean;
+}) => {
+  console.log("setting claims", data);
+  const authUser = await admin.auth().getUserByEmail(data.email); // .catch(v=>{console.log('exception',v)});
+  console.log("authUser", authUser.uid);
+  return admin.auth().setCustomUserClaims(authUser.uid, {
+    level: data.level,
+    enabled: data.enabled
+  });
+};
+
+exports.triggerUsers = functions.database.ref("/userProfile").onWrite(event => {
+  getUpdatedUser(event).forEach(user => {
+    const userData = event.after.val()[user];
+    const claims = {
+      email: userData.email,
+      level: userData.level,
+      enabled: userData.enabled
+    };
+    // tslint:disable-next-line: no-floating-promises
+    setClaims(claims)
+      .then(() => {
+        console.log("setted claims for ", userData.key);
+        console.log("seted claims", claims);
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+  });
+
+  // console.log('snapshot',event.after[event.after.key])
+});
