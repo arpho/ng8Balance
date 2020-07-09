da84d83c498afa0286399b390a9fa5c48d0fe82f
arpho <damicogiuseppe77@gmail.com>
2020-07-09 17:00:08 +0200

    implemented questionBarcode

---
+++ src/app/modules/dynamic-form/models/question-barcode.ts
@@ -0,0 +1,10 @@
+import { QuestionBase } from './question-base';
+import { QuestionProperties } from './questionproperties';
+
+export class QuestionBarcode extends QuestionBase<string>{
+    controlType:'barcodeScanner'
+    barcode:string
+    constructor(options:QuestionProperties<string>){
+        super(options)
+    }
+}
