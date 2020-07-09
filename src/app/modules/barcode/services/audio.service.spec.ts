14338acc6c1c285ebc1dd659cbe6664a69adc65b
arpho <damicogiuseppe77@gmail.com>
2020-07-09 11:11:11 +0200

    provided NativeAudio for Audio'service testing

--- src/app/modules/barcode/services/audio.service.spec.ts
+++ src/app/modules/barcode/services/audio.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AudioService } from './audio.service';
+import { NativeAudio } from '@ionic-native/native-audio/ngx';
 
 describe('AudioService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({providers:[NativeAudio]}));
 
   it('should be created', () => {
     const service: AudioService = TestBed.get(AudioService);
