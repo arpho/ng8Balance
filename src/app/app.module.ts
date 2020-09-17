dcc7f541c552374173de85efca2784e8e3c0123e
Giuseppe <damicogiuseppe77@gmail.com>
2020-09-17 18:12:37 +0200

    offlineModule
    generated module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,7 +46,7 @@ import { NativeAudio } from '@ionic-native/native-audio/ngx';
 import { UpdateFidelityCardPage } from './pages/update-fidelity-card/update-fidelity-card.page';
 import { CreateFidelityCardPage } from './pages/create-fidelity-card/create-fidelity-card.page';
 import { ScannerPopupPage } from './modules/barcode/pages/scanner-popup/scanner-popup.page';
-import * as PouchDB from 'pouchdb';
+import { OfflineModuleModule } from './modules/offline-module/offline-module.module';
 @NgModule({
   entryComponents: [
     CreateSupplierPage,
@@ -95,7 +95,7 @@ import * as PouchDB from 'pouchdb';
     CategoriesViewerComponent],
 
   imports: [
-    // PouchDB,
+    OfflineModuleModule,
     BarcodeModule,
     ItemModule,
     WidgetModule,
