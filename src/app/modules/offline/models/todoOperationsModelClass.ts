8b11e0d0be8e8ded3090cfb04ec228b3b94ab6dc
giuseppe <damicogiuseppe77@gmail.com>
2020-10-14 19:16:09 +0200

    some utility methods
    implemented sinchreonizedBy and hasBenSichronizedBy
    renamed sinchromizedBy in sinchronizedByList

--- src/app/modules/offline/models/todoOperationsModelClass.ts
+++ src/app/modules/offline/models/todoOperationsModelClass.ts
@@ -2,25 +2,54 @@ import { DateModel } from '../../user/models/birthDateModel'
 import { Operations } from './Operations'
 
 export class TodoOPeration {
+
   itemKey: string
+
   date: DateModel
+
   signature: string
+
   sinchronizedBy: [string]
+
   operation: Operations
+
+
   constructor(op: { itemKey: string, date: string, operation: Operations, signature: string }) {
+
     Object.assign(this, op)
+
     this.date = new DateModel(op.date)
 
   }
 
   serialize() {
+    
     return {
+
       'itemKey': this.itemKey,
+
       date: this.date.formatDate(),
+
       sinchronizedBy: this.sinchronizedBy,
+
       operation: this.operation,
+
       signature: this.signature
 
     }
+  }
+
+
+  sinchronize(signature: string) {
+
+    this.sinchronizedBy.push(signature)
+
+  }
+
+
+  hasBeenSinchronizedBy(signature: string) {
+
+    return this.sinchronizedBy.indexOf(signature) > -1
+
   }
 }
