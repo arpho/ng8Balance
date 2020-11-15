5b441cd1f0a666a1f3d4b80174f363eec01122fd
giuseppe <damicogiuseppe77@gmail.com>
2020-11-15 19:01:59 +0100

    cloud functions for categories
    implemented cloud functions for categories for  creation and updating

--- src/app/modules/offline/business/cloudFunctions.ts
+++ src/app/modules/offline/business/cloudFunctions.ts
@@ -2,9 +2,21 @@ import { Operations } from '../models/Operations';
 
 import * as firebase from 'firebase'
 import * as functions from 'firebase/functions'
+import { DateModel } from '../../user/models/birthDateModel';
 export class CloudFunctions {
   toBeSynchronized = (operation: Operations,
     entity: string,
     data: firebase.database.DataSnapshot,
-    context) => { }
+    context) => {
+    const today = new DateModel()
+
+    const ref = firebase.database().ref(`/toBeSynchronized/${context.params.uid}/`)
+    const item = {
+      entityKey: entity,
+      operation,
+      date:today.formatDate(),
+      item_key: data.key
+    }
+    ref.push(item)
+  }
 }
