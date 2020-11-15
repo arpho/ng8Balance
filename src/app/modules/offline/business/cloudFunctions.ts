89d1d6113ac256dd1c5f4369d59d8b021a7984c8
giuseppe <damicogiuseppe77@gmail.com>
2020-11-15 18:12:35 +0100

    refactoring
    added entity field to  toBesynchronized function

--- src/app/modules/offline/business/cloudFunctions.ts
+++ src/app/modules/offline/business/cloudFunctions.ts
@@ -3,5 +3,8 @@ import { Operations } from '../models/Operations';
 import * as firebase from 'firebase'
 import * as functions from 'firebase/functions'
 export class CloudFunctions {
-  toBeSynchronized = (operation: Operations, data: firebase.database.DataSnapshot, context) => { }
+  toBeSynchronized = (operation: Operations,
+    entity: string,
+    data: firebase.database.DataSnapshot,
+    context) => { }
 }
