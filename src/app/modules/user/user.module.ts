9bcc14bd16f3459fadddd171e951868e823421fc
arpho <damicogiuseppe77@gmail.com>
2020-12-27 11:45:57 +0100

    fixed login
    refactored the route to login page and imported formsModule and reactiveFormsModule in User Module

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginPage } from './pages/login/login.page';
 
 @NgModule({
-  declarations: [],
-  imports: [IonicModule.forRoot(),
+  declarations: [LoginPage],
+  imports: [FormsModule,ReactiveFormsModule,IonicModule.forRoot(),RouterModule.forChild([{path:'login',component:LoginPage}]),
     CommonModule
   ],
 })
