df06cfccf6a746843cc7e7056ab0810683c97eae
arpho <damicogiuseppe77@gmail.com>
2021-01-12 18:12:30 +0100

    OfflineItemModelInterface
    implemented first realese of the interface

---
+++ src/app/modules/offline/models/OffilineModelInterface.ts
@@ -0,0 +1,13 @@
+import { ItemModelInterface } from "../../item/models/itemModelInterface";
+
+export interface OfflineItemModelInterface extends ItemModelInterface{
+    offlineKey:string
+    dependeciesList:[string]
+    signature:string
+    coherent:boolean //state whether the item's key is coherent with the db
+    entityKey:string
+
+    hasDepencies():boolean
+    updateDendencies(entityKey:string,offlineKey:string,onlineKey)
+    isCoherent():boolean
+}
