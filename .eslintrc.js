166eceeaf90eadabb70b2c802e8b94e51b4326e9
arpho <damicogiuseppe77@gmail.com>
2021-01-30 20:10:24 +0100

    migration2eslint
    exported configuration

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {},
+    "plugin": ["@typescript-eslint"]
+}
